1a680661d195616e3e3c556446e74c8ff3bdd3ef
AmanKumarWeb19 <akgupta0524@gmail.com>
2024-04-04 14:02:05 +0530

    movie and detail router to check authentication

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -19,13 +19,35 @@ userRouter.post("/login", async (req, res) => {
   const { email, pass } = req.body;
   try {
     const user = await UserModel.find({ email, pass });
-    console.log(user);
-    res.send({ msg: "Login Successfully" });
+    if (user.length > 0) {
+      res.status(200).send({ msg: "Login Successfull!", token: "abc123" });
+    } else {
+      res.status(400).send({ msg: "Login failed!" });
+    }
   } catch (error) {
     res.send({ msg: "something went wrong", error: error.message });
   }
 });
 
+userRouter.get("/details", (req, res) => {
+  const { token } = req.query;
+
+  if (token === "abc123") {
+    res.status(200).send({ msg: "User Details" });
+  } else {
+    res.status(400).send({ msg: "access denied" });
+  }
+});
+
+userRouter.get("/movie", (req, res) => {
+  const { token } = req.query;
+
+  if (token === "abc123") {
+    res.status(200).send({ msg: "Movie Details" });
+  } else {
+    res.status(400).send({ msg: "login required cannot access" });
+  }
+});
 module.exports = {
   userRouter,
 };
