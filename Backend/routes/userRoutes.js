53e46fc60047c1698a5d5a74c749d123cef1c2ad
AmanKumarWeb19 <akgupta0524@gmail.com>
2024-04-05 09:33:07 +0530

    code practice and update some code in details and movie

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,53 +1,129 @@
+// const express = require("express");
+// const { UserModel } = require("../models/userModel");
+// const jwt = require("jsonwebtoken");
+// const userRouter = express.Router();
+
+// userRouter.post("/register", async (req, res) => {
+//   const payload = req.body;
+//   try {
+//     const user = new UserModel(payload);
+//     await user.save();
+//     res.send({ msg: "Register Successfully" });
+//   } catch (error) {
+//     res.send({ msg: "something went wrong", error: error.message });
+//   }
+// });
+
+// // ...............................................................................................
+
+// userRouter.post("/login", async (req, res) => {
+//   const { email, pass } = req.body;
+//   try {
+//     const user = await UserModel.find({ email, pass });
+//     if (user.length > 0) {
+//       const token = jwt.sign({ name: "batman" }, "bruce");
+//       res.status(200).send({ msg: "Login Successfull!", token: token });
+//     } else {
+//       res.status(400).send({ msg: "Login failed!" });
+//     }
+//   } catch (error) {
+//     res.send({ msg: "something went wrong", error: error.message });
+//   }
+// });
+
+// userRouter.get("/details", (req, res) => {
+//   const token = req.headers.authorization;
+//   jwt.verify(token, "bruce", (err, decoded) => {
+//     if (decoded) {
+//       res.status(200).send({ msg: "User Details" });
+//     } else {
+//       res.status(400).send({ msg: err.message });
+//     }
+//   });
+// });
+
+// userRouter.get("/movie", (req, res) => {
+//   const token = req.headers.authorization;
+//   jwt.verify(token, "bruce", (err, decoded) => {
+//     if (decoded) {
+//       res.status(200).send({ msg: "Movie Details" });
+//     } else {
+//       res.status(400).send({ msg: "access denied" });
+//     }
+//   });
+// });
+
+// module.exports = {
+//   userRouter,
+// };
+
 const express = require("express");
-const { UserModel } = require("../models/userModel");
 const userRouter = express.Router();
+const jwt = require("jsonwebtoken");
+const { UserModel } = require("../models/userModel");
 
 userRouter.post("/register", async (req, res) => {
   const payload = req.body;
   try {
     const user = new UserModel(payload);
     await user.save();
-    res.send({ msg: "Register Successfully" });
+    res.send({ msg: "register Successful!" });
   } catch (error) {
-    res.send({ msg: "something went wrong", error: error.message });
+    res.status(400).send({ msg: "Can't Register!", error: error.message });
   }
 });
 
-// ...............................................................................................
+/**................................................................................................ */
 
 userRouter.post("/login", async (req, res) => {
   const { email, pass } = req.body;
   try {
     const user = await UserModel.find({ email, pass });
     if (user.length > 0) {
-      res.status(200).send({ msg: "Login Successfull!", token: "abc123" });
+      const token = jwt.sign({ name: "aman" }, "bruce");
+      res.send({ msg: "Login Successful!", token: token });
     } else {
-      res.status(400).send({ msg: "Login failed!" });
+      res.status(400).send({ msg: "Can't able Login!", error: error.message });
     }
   } catch (error) {
-    res.send({ msg: "something went wrong", error: error.message });
+    res.status(400).send({ msg: "Can't Login!", error: error.message });
   }
 });
+
+/**................................................................................................ */
 
 userRouter.get("/details", (req, res) => {
-  const { token } = req.query;
-
-  if (token === "abc123") {
-    res.status(200).send({ msg: "User Details" });
-  } else {
-    res.status(400).send({ msg: "access denied" });
+  const token = req.headers.authorization;
+  try {
+    jwt.verify(token, "bruce", (err, decoded) => {
+      if (decoded) {
+        res.send({ msg: "Details Page" });
+      } else {
+        res.send({ msg: "Login First!", err: err.message });
+      }
+    });
+  } catch (error) {
+    res.send({ msg: "Access denied Login First!", error: error.message });
   }
 });
+
+/**............................................................................................... */
 
 userRouter.get("/movie", (req, res) => {
-  const { token } = req.query;
-
-  if (token === "abc123") {
-    res.status(200).send({ msg: "Movie Details" });
-  } else {
-    res.status(400).send({ msg: "login required cannot access" });
+  const token = req.headers.authorization;
+  try {
+    jwt.verify(token, "bruce", (err, decoded) => {
+      if (decoded) {
+        res.send({ msg: "Movie Page" });
+      } else {
+        res.send({ msg: "Login First!", err: err.message });
+      }
+    });
+  } catch (error) {
+    res.send({ msg: "Access denied Login First!", error: error.message });
   }
 });
+
 module.exports = {
   userRouter,
 };
